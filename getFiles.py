539003ced4ec9b2b87b1f2f637e781671f9af290
Michael Giansiracusa <giansiracumt@ornl.gov>
2019-02-13 14:35:23 -0500

    imports to work with python 2 and 3

--- getFiles.py
+++ getFiles.py
@@ -10,6 +10,16 @@ Purpose:
 Requirements:
     This tool requires python3, requests, and loguru package
 """
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+from builtins import int
+try:
+    from future_builtins import ascii, filter, hex, map, oct, zip
+except:
+    pass
+import sys
+if sys.version_info.major > 2:
+    xrange = range
 import argparse
 import json
 import requests

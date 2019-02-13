e06ec4e496523d319adbdb2c1ab02657663740b9
Michael Giansiracusa <giansiracumt@ornl.gov>
2019-02-13 14:56:39 -0500

    fixing ModuleNotFoundError

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     ],
     entry_points = {
             'console_scripts': [
-                'getFiles = getFiles:main',
+                'getFiles = src.getFiles:main',
             ],
         }
 )

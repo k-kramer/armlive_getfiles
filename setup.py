4e9db34dcf8a5f6e0b9175e20b49f1222e9f0e4f
Michael T. Giansiracusa <giansiracumt@ornl.gov>
2019-11-06 12:21:10 -0500

    added version info to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='armlive: get files',
     description='Auto downloader for using armlive web service from ADC',
+    version='1.2.0',
     url='https://adc.arm.gov/armlive/',
     author='Michael Giansiracusa',
     author_email='giansiracumt@ornl.gov',
